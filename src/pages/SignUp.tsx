daea8c2de07c848660a992d1879fea9c97842348
Purpose11 <adebayofatai924@gmail.com>
2023-11-16 16:09:41 +0100

    test

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,12 +4,12 @@ import { AiOutlineMail } from "react-icons/ai";
 import { CiLock } from "react-icons/ci";
 const SignUp = () => {
   return (
-    <div className="w-full lg:h-[691px] h-[747px] flex flex-col lg:flex-row lg:gap-[60px] gap-0">
+    <div className="w-full lg:h-[691px] h-fit flex flex-col lg:flex-row lg:gap-[60px] gap-0">
       <img
         src={space2}
         className="lg:w-[610px] w-full lg:h-[691px] h-[232px]"
       />
-      <div className="lg:w-[610px] w-[315px] lg:h-[691px] h-[515px] mx-auto lg:mx-0 lg:py-[100px] py-[30px]">
+      <div className="lg:w-[610px] w-[315px] lg:h-[691px] h-fit mx-auto lg:mx-0 lg:py-[100px] py-[30px] border">
         <div className="lg:w-[460px] w-[315px] lg:h-[146px] h-[110px]">
           <h2 className=" font-Work_sance lg:text-[51px] text-[38px] leading-[45.6px] lg:leading-[56.1px] font-[600] capitalize">
             Create account
@@ -19,7 +19,7 @@ const SignUp = () => {
             selling NFTs.
           </p>
         </div>
-        <div className="lg:w-[330px] w-[315px] h-[305px] lg:mt-[40px] mt-[30px]">
+        <div className="lg:w-[330px] w-[315px] h-[305px] lg:mt-[40px] mt-[30px] border">
           <div className="w-full h-[229px] flex flex-col gap-[15px] ">
             <div className="bg-white w-full h-[46px] rounded-[20px] px-[20px] flex items-center gap-[12px] text-[#2B2B2B]">
               <PiUser className="w-[20px] h-[20px] text-gray-500" />
